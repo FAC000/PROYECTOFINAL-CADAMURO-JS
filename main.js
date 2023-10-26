80f8d7ca01978a8d12769109137789e44a37182a
Facundo Cad <facundocad1@gmail.com>
2023-10-26 16:43:10 -0300

    TERCER PREENTREGA

--- main.js
+++ main.js
@@ -1,82 +1,100 @@
-class conversor {
-    constructor(cambio, compra, venta) {
-        this.cambio = cambio;
-        this.compra = compra;
-        this.venta = venta;
-    }
 
-    hablar() {
-        alert("ESTA ES LA INFORMACION DEL" + " " + this.cambio + ":" + " " + "COMPRA" + " " + this.compra + " " + "-" + " " + "VENTA" + " " + " " + this.venta);
-    }
-}
+// CODIGO DE LA CALCULADORA
 
-const dolarinformacion = new conversor("DOLAR", 1010, 990);
-const euroinformacion = new conversor("EURO", 397, 377);
-
-const actualizacion = new Date()
-alert("ULTIMA ACTUALIZACION DE PRECIOS:" + " " + actualizacion.toLocaleDateString())
-
-euroinformacion.hablar();
-dolarinformacion.hablar();
-
-
-while (seguiroperando != "NO") {
-    let dolar = 990
-    let euro = 372
-
-    const monedas = [{
-            nombre: 'EURO',
-            precio: 397
-        },
-        {
-            nombre: 'DOLAR',
-            precio: 990
-        },
-    ]
-
-    const variasmonedas = monedas.map((el) => el.nombre)
-
-    alert("A CONTINUACION LAS MONEDAS CON LAS QUE TRABAJAMOS")
-    alert(variasmonedas)
-
-
-
-let tipodecambio = prompt("INGRESE SU TIPO DE CAMBIO DOLAR O EURO").toUpperCase()
-
+let dolar = 1000
+let euro = 372
 
 function solicitarMontodolar() {
-    let montoIngresado = prompt("INGRESAR MONTO")
-    if (montoIngresado > 0)
-        alert((montoIngresado / dolar).toFixed(2));
+    let contadordepesos = document.getElementById("contadordepesos")
+    swal.fire({
+        title: 'EL RESULTADO DE TU CONVERSION ES:',
+        icon: 'success',
+        text: ((contadordepesos.value / dolar).toFixed(2) + "USD"),
+        background: '#f6d2a0',
+        color: '#bc5761cb',
+        iconColor: 'brown',
+        confirmButtonColor: '#512c2b'
+    })
 }
 
 function solicitarMontoeuro() {
-    let montoIngresado = prompt("INGRESAR MONTO")
-    if (montoIngresado > 0)
-        alert((montoIngresado / euro).toFixed(2));
+    let contadordepesos = document.getElementById("contadordepesos")
+    swal.fire({
+        title: 'EL RESULTADO DE TU CONVERSION ES:',
+        icon: 'success',
+        text: ((contadordepesos.value / euro).toFixed(2) + "EUR"),
+        background: '#f6d2a0',
+        color: '#bc5761cb',
+        iconColor: 'brown',
+        confirmButtonColor: '#512c2b'
+    })
 }
 
-function alertaopcioninvalida() {
-    alert("USTED ESTA INGRESANDO UNA OPCION INVALIDA")
+let botondolar = document.getElementById("botondolar")
+botondolar.addEventListener('click', solicitarMontodolar)
+
+let botoneuro = document.getElementById("botoneuro")
+botoneuro.addEventListener('click', solicitarMontoeuro)
+
+// CODIGO INFORMACION DEL TIPO DE CAMBIO
+
+const productos = [{
+    moneda: "DOLAR",
+    compra: 960,
+    venta: 980
+},
+{
+    moneda: "EURO",
+    compra: 375,
+    venta: 375
+},
+];
+
+
+
+for (const producto of productos) {
+let contenedor = document.createElement("div");
+contenedor.innerHTML = `<h3> MONEDA: ${producto.moneda}</h3>
+                    <p>  COMPRA $ ${producto.compra}</p>
+                    <p> VENTA $ ${producto.venta}</p>`;
+document.getElementById("cotizaciones").appendChild(contenedor).classList.add("container");
+
 }
 
+// CODIGO FORMULARIO
 
-switch (tipodecambio) {
+let formulario = document.getElementById("formulario")
+let nombreusuario = document.getElementById("nombre")
+let emailusuario = document.getElementById("email")
 
-    case "DOLAR":
-        alert("USTED ELIGIO DOLAR")
-        solicitarMontodolar()
-        break;
-
-    case "EURO":
-        alert("USTED ELIGIO EURO")
-        solicitarMontoeuro()
-        break;
+function guardarformulario() {
+    localStorage.setItem("nombre", nombreusuario.value)
+    localStorage.setItem("email", emailusuario.value)
 }
 
-var seguiroperando = prompt("DESEA SEGUIR OPERANDO? SI O NO").toUpperCase()
- if ( seguiroperando != "SI"){
-    alert ("GRACIAS POR ELEGIRNOS")
-    break
- }
+let enviando = document.getElementById("enviando")
+
+function aviso() {
+    enviando.innerHTML = "<h4>TU FORMULARIO FUE ENVIADO,GRACIAS!</h4>"
 }
+
+function avisodecompletar() {
+    enviando.innerHTML = "<h4>COMPLETA TODAS LAS CASILLAS.</h4>"
+}
+
+let enviarformulario = document.getElementById("enviar")
+enviarformulario.addEventListener('click', function (event) {
+    event.preventDefault();
+    guardarformulario()
+    let completarformulario = (emailusuario.value) ? true : false
+    completarformulario ? aviso() : avisodecompletar()
+    nombreusuario.value == "" && avisodecompletar()
+    formulario.reset();
+})
+
+// CODIGO FECHA DE ACTUALIZACION
+
+const actualizacion = new Date()
+let container_ = document.createElement("div");
+container_.innerHTML = `<h3> ULTIMA FECHA DE ACTUALIZACION ${actualizacion}</h3>`;
+document.body.appendChild(container_).classList.add("container_");
