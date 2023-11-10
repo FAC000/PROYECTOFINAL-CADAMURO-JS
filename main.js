eee381f49901208ecf9f48b8c7eb3fb7b403149d
Facundo Cad <facundocad1@gmail.com>
2023-11-10 09:43:50 -0300

    ENTREGA FINAL

--- main.js
+++ main.js
@@ -1,96 +1,162 @@
-
 // CODIGO DE LA CALCULADORA
 
-let dolar = 1000
-let euro = 372
+// DOLAR
+fetch("https://dolarapi.com/v1/dolares/blue")
+    .then(response => response.json())
+    .then((data) => {
 
-function solicitarMontodolar() {
-    let contadordepesos = document.getElementById("contadordepesos")
-    swal.fire({
-        title: 'EL RESULTADO DE TU CONVERSION ES:',
-        icon: 'success',
-        text: ((contadordepesos.value / dolar).toFixed(2) + "USD"),
-        background: '#f6d2a0',
-        color: '#bc5761cb',
-        iconColor: 'brown',
-        confirmButtonColor: '#512c2b'
+        function solicitarMontodolar() {
+            let contadordepesos = document.getElementById("contadordepesos")
+            swal.fire({
+                title: 'EL RESULTADO DE TU CONVERSION ES:',
+                icon: 'success',
+                text: ((contadordepesos.value / (data.compra)).toFixed(2) + "USD"),
+                background: '#f1827a',
+                color: '#f8efb2',
+                iconColor: '#efc562',
+                confirmButtonColor: '#efc562'
+            })
+        }
+        
+       
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML = `<h3> MONEDA: ${data.moneda}</h3>
+                        <p>  COMPRA $ ${data.compra}</p>
+                        <p> VENTA $ ${data.venta}</p>`;
+        document.getElementById("cotizaciones").appendChild(contenedor).classList.add("container");
+    
+    
+
+        let botondolar = document.getElementById("botondolar")
+        botondolar.addEventListener('click', solicitarMontodolar)
+
+        .catch (()=>console.error("Error"))
+       
     })
-}
 
-function solicitarMontoeuro() {
-    let contadordepesos = document.getElementById("contadordepesos")
-    swal.fire({
-        title: 'EL RESULTADO DE TU CONVERSION ES:',
-        icon: 'success',
-        text: ((contadordepesos.value / euro).toFixed(2) + "EUR"),
-        background: '#f6d2a0',
-        color: '#bc5761cb',
-        iconColor: 'brown',
-        confirmButtonColor: '#512c2b'
+// EURO    
+fetch("https://dolarapi.com/v1/cotizaciones/eur")
+    .then(response => response.json())
+    .then((data) => {
+
+        function solicitarMontoeuro() {
+            let contadordepesos = document.getElementById("contadordepesos")
+            swal.fire({
+                title: 'EL RESULTADO DE TU CONVERSION ES:',
+                icon: 'success',
+                text: ((contadordepesos.value / (data.compra)).toFixed(2) + "EUR"),
+                background: '#f1827a',
+                color: '#f8efb2',
+                iconColor: '#efc562',
+                confirmButtonColor: '#efc562'
+            })
+        }
+
+            let contenedor = document.createElement("div");
+            contenedor.innerHTML = `<h3> MONEDA: ${data.moneda}</h3>
+                            <p>  COMPRA $ ${data.compra}</p>
+                            <p> VENTA $ ${data.venta}</p>`;
+            document.getElementById("cotizaciones").appendChild(contenedor).classList.add("container");
+        
+        
+
+        let botoneuro = document.getElementById("botoneuro")
+        botoneuro.addEventListener('click', solicitarMontoeuro)
+
+        .catch (()=>console.error("Error"))
+       
     })
-}
 
-let botondolar = document.getElementById("botondolar")
-botondolar.addEventListener('click', solicitarMontodolar)
-
-let botoneuro = document.getElementById("botoneuro")
-botoneuro.addEventListener('click', solicitarMontoeuro)
 
 // CODIGO INFORMACION DEL TIPO DE CAMBIO
 
-const productos = [{
-    moneda: "DOLAR",
-    compra: 960,
-    venta: 980
-},
-{
-    moneda: "EURO",
-    compra: 375,
-    venta: 375
-},
-];
 
-
-
-for (const producto of productos) {
-let contenedor = document.createElement("div");
-contenedor.innerHTML = `<h3> MONEDA: ${producto.moneda}</h3>
-                    <p>  COMPRA $ ${producto.compra}</p>
-                    <p> VENTA $ ${producto.venta}</p>`;
-document.getElementById("cotizaciones").appendChild(contenedor).classList.add("container");
-
-}
 
 // CODIGO FORMULARIO
 
-let formulario = document.getElementById("formulario")
-let nombreusuario = document.getElementById("nombre")
-let emailusuario = document.getElementById("email")
+
+
+const nombre = document.getElementById("nombre")
+const email = document.getElementById("email")
+const form = document.getElementById("formulario")
+const parrafo = document.getElementById("warnings")
 
 function guardarformulario() {
-    localStorage.setItem("nombre", nombreusuario.value)
-    localStorage.setItem("email", emailusuario.value)
+    localStorage.setItem("nombre", nombre.value)
+    localStorage.setItem("email", email.value)
 }
 
-let enviando = document.getElementById("enviando")
+
 
 function aviso() {
-    enviando.innerHTML = "<h4>TU FORMULARIO FUE ENVIADO,GRACIAS!</h4>"
+    Toastify({
+        text: "FORMULARIO ENVIADO 👌",
+        className: "succes",
+        duration: 4000,
+        style: {
+            background: "#f8efb2",
+            color: "black"
+        }
+    }).showToast();
+
+
 }
 
-function avisodecompletar() {
-    enviando.innerHTML = "<h4>COMPLETA TODAS LAS CASILLAS.</h4>"
+function alerta() {
+    entrar = true;
+    parrafo.innerHTML = "<h4> el nombre es invalido, agregar mas de 4 caracteres.</h4>"
+    setTimeout(()=> {
+        parrafo.innerHTML = " "
+    }, 4000)
 }
 
-let enviarformulario = document.getElementById("enviar")
-enviarformulario.addEventListener('click', function (event) {
-    event.preventDefault();
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    nombre.value.length < 4 && alerta()
+        (!regexEmail.test(email.value)) && alerta()
+    const completarformulario = (entrar = parrafo.innerHTML = warnings) ? true : false
+    completarformulario ? parrafo.innerHTML = warnings : aviso()
     guardarformulario()
-    let completarformulario = (emailusuario.value) ? true : false
-    completarformulario ? aviso() : avisodecompletar()
-    nombreusuario.value == "" && avisodecompletar()
-    formulario.reset();
+    form.reset();
 })
+
+// API 
+
+fetch("https://dolarapi.com/v1/dolare")
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data)
+        data.forEach((info) => {
+                let contenedorapi = document.createElement("div");
+                contenedorapi.innerHTML =
+                    `
+        <p> ${info.nombre}</p>
+        <p> COMPRA  ${info.compra}</p>
+        <p> VENTA ${info.venta}</p>
+        `
+                document.getElementById("api").appendChild(contenedorapi).classList.add("containerapi");
+
+            })
+            .catch (()=>console.error("Error"))
+    })
+
+// MONEDAS PRINCIPALES ARG
+
+
+
+const monedas = ["EURO", "DOLAR", "DOLAR AUSTRALIANO", "LIBRA", "YEN"]
+
+let monedasdiv = document.createElement("div");
+monedasdiv.innerHTML =
+    `
+  <h1 class= "titulo">monedas de mayor circulacion en arg</h1>
+  <p> ${monedas.join("-")} </p>
+  
+  `
+document.getElementById("monedas").appendChild(monedasdiv).classList.add("monedasdiv");
+
 
 // CODIGO FECHA DE ACTUALIZACION
 
@@ -98,3 +164,4 @@ const actualizacion = new Date()
 let container_ = document.createElement("div");
 container_.innerHTML = `<h3> ULTIMA FECHA DE ACTUALIZACION ${actualizacion}</h3>`;
 document.body.appendChild(container_).classList.add("container_");
+
